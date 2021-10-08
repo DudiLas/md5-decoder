eb8937cab1810bdbcdb4d47f979421059da69a4c
unknown <goldihon@gmail.com>
2021-10-08 10:55:47 +0300

    basic decoding loop

--- basic_decryptor.py
+++ basic_decryptor.py
@@ -1,14 +1,31 @@
 import hashlib
+import random
 
-class encoder():
+class md5_worker():
 
     def encode(self,str):
         encoded_data = hashlib.md5(str.encode())
         return encoded_data.hexdigest()
 
+    def decode(self, data, len):
+        ranged = 10 ** len
+        for num in range(ranged):
+            str_num = str(num)
+            encoded_str = self.encode(str_num)
+            if encoded_str == data:
+                return str_num
+
+
+
+
 
 if __name__ == "__main__":
-    str = "dudi"
-    encoder = encoder()
-    encoded_string = encoder.encode(str)
-    print(encoded_string)
+    st = str(random.randint(0,100000))
+    print("str to decode: ", st)
+    worker = md5_worker()
+    data_to_decrypt = worker.encode(st)
+
+    print("decoded string: ", worker.decode(data_to_decrypt, len(st)))
+
+
+
